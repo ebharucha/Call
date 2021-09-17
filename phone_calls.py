dbe7a33cdd898a47dbc9f944593536947452f1bc
ebharucha <ebharucha@gmail.com>
2021-09-17 18:37:50 +0800

    initial commit

---
+++ phone_calls.py
@@ -0,0 +1,22 @@
+import os
+from twilio.rest import Client
+
+# Twilio phone number goes here. Grab one at https://twilio.com/try-twilio
+# and use the E.164 format, for example: "+12025551234"
+# TWILIO_PHONE_NUMBER = "+15126451472"
+
+# account_sid = os.environ['TWILIO_ACCOUNT_SID']
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+# print (account_sid)
+# print (auth_token)
+
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+                        twiml='<Response><Say>Ahoy, World!</Say></Response>',
+                        to='+6596467552',
+                        from_='+15126451472'
+                    )
+
+print(call.sid)
